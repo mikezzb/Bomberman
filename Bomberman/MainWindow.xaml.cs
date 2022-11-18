2ff09ac410e0dba8ab1a4eab1f83e5faad071ffa
Mike Zhou <mikezhoudev@gmail.com>
2022-11-19 00:06:16 +0800

    feat: home page & frame navigation

--- Bomberman/MainWindow.xaml.cs
+++ Bomberman/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
 
 namespace Bomberman
 {
@@ -20,9 +10,16 @@ namespace Bomberman
   /// </summary>
   public partial class MainWindow : Window
   {
+    public static MainWindow Instance;
     public MainWindow()
     {
       InitializeComponent();
+      SwitchView(new HomePage());
+      Instance = this;
+    }
+
+    public void SwitchView(Page content) { 
+      frameNavigation.Content = content;
     }
   }
 }
