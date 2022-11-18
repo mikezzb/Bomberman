009d2bcea541ea2930d9c91432948732b4fd8b1f
Mike Zhou <mikezhoudev@gmail.com>
2022-11-18 20:20:15 +0800

    feat: game window

---
+++ Bomberman/GameWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using Bomberman.GameEngine;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bomberman
+{
+  /// <summary>
+  /// Interaction logic for Window1.xaml
+  /// </summary>
+  public partial class GameWindow : Window
+  {
+    public GameWindow()
+    {
+      InitializeComponent();
+      Debug.WriteLine("Test");
+      // bind events
+      gameWindow.KeyDown += GameController.Instance.KeyDownHandler;
+    }
+  }
+}
