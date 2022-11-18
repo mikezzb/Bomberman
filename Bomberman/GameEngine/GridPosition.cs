6ab342614b5981b85e96bab6039f4b7f8643b6f8
Mike Zhou <mikezhoudev@gmail.com>
2022-11-19 03:34:42 +0800

    feat: grid position class

---
+++ Bomberman/GameEngine/GridPosition.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+
+namespace Bomberman.GameEngine
+{
+  public class GridPosition
+  {
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public double OffsetX { get; private set; }
+    public double OffsetY { get; private set; }
+    public Point CanvasPosition
+    {
+      get => new Point(CanvasX, CanvasY);
+    }
+    public double PreciseX
+    {
+      get => (X + OffsetX);
+    }
+    public double PreciseY
+    {
+      get => (Y + OffsetY);
+    }
+    public double CanvasX
+    {
+      get => PreciseX * Config.ItemSize;
+    }
+    public double CanvasY
+    {
+      get => PreciseY * Config.ItemSize;
+    }
+
+    public GridPosition(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+    public void SetX(int x)
+    {
+      X = x;
+    }
+    public void SetY(int y)
+    {
+      Y = y;
+    }
+    public void ShiftX(double dx)
+    {
+      SetX(PreciseX + dx);
+    }
+    public void ShiftY(double dy)
+    {
+      SetY(PreciseY + dy);
+    }
+    public void SetX(double x)
+    {
+      X = (int)Math.Truncate(x);
+      OffsetX = x - X;
+    }
+    public void SetY(double y)
+    {
+      Y = (int)Math.Truncate(y);
+      OffsetY = y - Y;
+    }
+  }
+}
---
+++ BombermanTests/GameEngine/GridPositionTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bomberman.GameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomberman.GameEngine.Tests
+{
+  [TestClass()]
+  public class GridPositionTests
+  {
+    /// <summary>
+    /// Black box test
+    /// </summary>
+    [TestMethod()]
+    public void GridPositionIntTest()
+    {
+      GridPosition gp = new(7, 3);
+      gp.SetX(13);
+      gp.SetY(1);
+      Assert.AreEqual(520, gp.CanvasPosition.X);
+      Assert.AreEqual(40, gp.CanvasPosition.Y);
+    }
+    /// <summary>
+    /// Black box test
+    /// </summary>
+    [TestMethod()]
+    public void GridPositionFloatTest()
+    {
+      GridPosition gp = new(7, 3);
+      gp.ShiftX(0.1);
+      gp.ShiftY(1.5);
+      Assert.AreEqual(284, gp.CanvasPosition.X);
+      Assert.AreEqual(180, gp.CanvasPosition.Y);
+    }
+  }
+}
