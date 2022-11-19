91f369e13b6a14d786ddc44997268e0782434d17
Mike Zhou <mikezhoudev@gmail.com>
2022-11-19 16:22:02 +0800

    feat: config & enum

---
+++ Bomberman/GameEngine/Config.cs
@@ -0,0 +1,25 @@
+﻿namespace Bomberman.GameEngine
+{
+  public static class Config
+  {
+    public static readonly int ItemSize = 40;
+    public static readonly int Height = 13;
+    public static readonly int Width = 31;
+    public static readonly string ImageExt = ".png";
+    public static readonly string Map = Resources.map;
+  };
+  namespace Enums
+  {
+    public enum GameObject
+    {
+      Wall = '#',
+      Floor = ' ',
+    }
+    public enum GameState
+    {
+      Started,
+      Paused,
+      Ended,
+    }
+  }
+}
