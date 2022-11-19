09320db0eee2d92e2d426f2b57745dd20d7f91fd
Mike Zhou <mikezhoudev@gmail.com>
2022-11-19 16:23:42 +0800

    feat: util class

---
+++ Bomberman/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bomberman
+{
+  public static class Utilities
+  {
+    public static string ToMMSS(int seconds)
+    {
+      TimeSpan time = TimeSpan.FromSeconds(seconds);
+      return time.ToString(@"mm\:ss");
+    }
+  }
+}
