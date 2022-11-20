359a1d342f711ce4f49032abb24253ca463f046a
Mike Zhou <mikezhoudev@gmail.com>
2022-11-20 23:47:35 +0800

    feat: movable map object

---
+++ Bomberman/GameEngine/MapObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Bomberman.GameEngine
+{
+  /// <summary>
+  /// Abstract class of map object in game, including everything visible on the map
+  /// <list type="bullet">
+  /// <item>Position</item>
+  /// <item>Display (Image)</item>
+  /// </list>
+  /// </summary>
+  public abstract class MapObject
+  {
+    protected GridPosition position;
+    protected Sprite sprite;
+    /// <summary>
+    /// Do nothing constructor for inherited class to define their own
+    /// </summary>
+    protected MapObject() { }
+    protected MapObject(int x, int y, string name, Dictionary<string, int?>? variant, string? defaultVariant)
+    {
+      position = new GridPosition(x, y);
+      sprite = new Sprite(name, variant, defaultVariant, ref position);
+    }
+    /// <summary>
+    /// Dispose a map object in GUI, perform cleanup actions
+    /// </summary>
+    public virtual void Dispose()
+    {
+      return;
+    }
+  }
+}
--- Bomberman/GameEngine/MapObjects/MovableMapObject.cs
+++ Bomberman/GameEngine/MapObjects/MovableMapObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Bomberman.GameEngine.Enums;
 
@@ -14,42 +15,84 @@ namespace Bomberman.GameEngine
   public abstract class MovableMapObject : MapObject
   {
     private double speed;
-    private Direction? currDirection;
-    protected MovableMapObject(int x, int y, string name, Dictionary<string, int>? variant, string? defaultVariant, double speed = 1.0) : base(x, y, name, variant, defaultVariant)
+    private double Speed
     {
-      this.speed = speed;
+      get => speed;
+      set
+      {
+        speed = value;
+        Debug.WriteLine($"Setting speed to {speed}");
+        moveAnimator.SetFrameDuration(WalkFrameDuration);
+      }
     }
-    private double WalkTime { get { return Config.UnitWalkTime * speed } }
+    private Direction? currDirection;
+    private Animator moveAnimator;
+    private bool stopAfterWalk = false;
+    private readonly AnimatedSprite animatedSprite;
+    private readonly MovableGridPosition movablePosition;
+    protected MovableMapObject(int x, int y, string name, Dictionary<string, int?>? variant, string? defaultVariant, double speed = 1.0) : base()
+    {
+      position = new MovableGridPosition(x, y);
+      sprite = new AnimatedSprite(name, variant, defaultVariant, ref position);
+      animatedSprite = (AnimatedSprite)sprite;
+      movablePosition = (MovableGridPosition)position;
+      moveAnimator = new Animator(WalkFrameDuration, Config.WalkFrames, OnAnimationTick);
+      Speed = speed;
+    }
+    private int WalkFrameDuration { get => (int)(Config.WalkFrameDuration * speed); }
+    private double WalkFrameLength { get => 1.0 / Config.WalkFrames; }
     /// <summary>
     /// Each walk is 1 unit only
     /// </summary>
     public void StartWalk(Direction dir)
     {
-      if (dir == currDirection) return;
+      if (currDirection != null) return;
+      stopAfterWalk = false;
       currDirection = dir;
       string directionName = Config.DirectionName[dir];
-      sprite.SwitchImage(directionName);
-      sprite.StartAnimation();
+      animatedSprite.SwitchImage(directionName);
+      animatedSprite.StartAnimation();
+      moveAnimator.Start();
     }
     /// <summary>
-    /// Stop Walk
+    /// Stop Walk (but still will finish current walk)
     /// </summary>
     public void StopWalk() {
-      Debug.WriteLine("Stopped walk");
-      sprite.StopAnimation();
-      currDirection = null;
+      Debug.WriteLine("Stopped walk scheduled");
+      stopAfterWalk = true;
     }
     public void SetSpeed(double speed)
     {
-      this.speed = speed;
+      Speed = speed;
     }
     public void SpeedUp()
     {
-      speed += 0.2;
+      Speed += 0.2;
     }
     public void SpeedDown()
     {
-      speed -= 0.2;
+      Speed -= 0.2;
+    }
+    /// <summary>
+    /// Draw displacement as walking
+    /// </summary>
+    private void OnAnimationTick(int frameNum)
+    {
+      if (currDirection == null) return;
+      bool stopAnimation = (stopAfterWalk && frameNum == 0);
+      // shift position & draw
+      movablePosition.Move(currDirection, WalkFrameLength);
+      animatedSprite.DrawUpdate();
+      if (stopAnimation) StopWalkAnimation();
+    }
+    private void StopWalkAnimation()
+    {
+      Debug.WriteLine("Stop Walk Animation");
+      animatedSprite.StopAnimation();
+      movablePosition.ResetOffset();
+      moveAnimator.Stop();
+      currDirection = null;
+      stopAfterWalk = false;
     }
   }
 }
