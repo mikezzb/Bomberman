4bdcb95ce814e53be38be5176f85f7467770502f
Mike Zhou <mikezhoudev@gmail.com>
2022-11-20 23:46:58 +0800

    feat: animated sprite

---
+++ Bomberman/GameEngine/Graphics/AnimatedSprite.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bomberman.GameEngine
+{
+  public class AnimatedSprite : Sprite
+  {
+    private int? currFrameNum;
+    private Animator? animator;
+    public Animator Animator
+    {
+      get
+      {
+        if (animator == null) animator = new Animator(100, 2, OnTick); return animator;
+      }
+    }
+    /// <summary>
+    /// key: variantName, value: frames of the variant
+    /// </summary>
+    private readonly Dictionary<string, List<BitmapImage>> animatedImages = new();
+    public AnimatedSprite(
+      string name,
+      Dictionary<string, int?>? variant,
+      string? defaultVariant,
+      ref GridPosition position
+      ) : base(name, variant, defaultVariant, ref position)
+    {
+    }
+    protected override void UpdateImage()
+    {
+      if (currFrameNum == null) base.UpdateImage();
+      else imageBrush.ImageSource = animatedImages[currVariant][(int)currFrameNum];
+    }
+    public override void SwitchImage(string variant)
+    {
+      // if static image
+      if (images.ContainsKey(variant))
+      {
+        base.SwitchImage(variant);
+        currFrameNum = null;
+      }
+      else
+      {
+        currVariant = variant;
+        currFrameNum = 0;
+        UpdateImage();
+        DrawUpdate();
+      }
+    }
+    public void StartAnimation()
+    {
+      Animator.Start();
+    }
+    public void StopAnimation()
+    {
+      Animator.Stop();
+    }
+    private void OnTick(int frameNum)
+    {
+      if (currFrameNum != frameNum)
+      {
+        currFrameNum = frameNum;
+        UpdateImage();
+      }
+      Debug.WriteLine("Sprite ticking");
+      DrawUpdate();
+    }
+    protected override void LoadImages()
+    {
+      // Mount all images
+      foreach (KeyValuePair<string, int?> pair in variant)
+      {
+        if (pair.Value == null)
+        {
+          images[pair.Key] = GetImage(GetImageUri(pair.Key));
+        }
+        else
+        {
+          List<BitmapImage> li = new List<BitmapImage>();
+          for (int frameNum = 1; frameNum <= pair.Value; frameNum++)
+          {
+            li.Add(GetImage(GetAnimatedImageUri(pair.Key, frameNum)));
+          }
+          animatedImages[pair.Key] = li;
+        }
+      }
+      DrawElement();
+    }
+    /// <summary>
+    /// As movable object shall display above background, it's default zIndex is 2
+    /// </summary>
+    /// <param name="zIndex"></param>
+    public override void DrawElement(int zIndex = 2)
+    {
+      base.DrawElement(zIndex);
+    }
+    private Uri GetAnimatedImageUri(string variantName, int frameNum)
+    {
+      return frameNum == 0 ?
+        GetImageUri(variantName) :
+        GetImageUriFromName($"{name}_{variantName}{frameNum}");
+    }
+    private static Uri GetImageUriFromName(string name)
+    {
+      return new Uri("Resources/" + name + Config.ImageExt, UriKind.Relative);
+    }
+  }
+}
---
+++ Bomberman/GameEngine/Graphics/Sprite.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bomberman.GameEngine
+{
+  public class Sprite : IDisposable
+  {
+    protected readonly string name;
+    protected readonly Dictionary<string, int?> variant;
+    protected readonly string defaultName;
+    protected string currVariant;
+    protected GridPosition position;
+    protected ImageBrush imageBrush = new ImageBrush();
+    protected UIElement canvasImage;
+    /// <summary>
+    /// key: variantName
+    /// </summary>
+    protected readonly Dictionary<string, BitmapImage> images = new();
+    public Sprite(
+      string name,
+      Dictionary<string, int?>? variant,
+      string? defaultVariant,
+      ref GridPosition position
+      )
+    {
+      this.name = name;
+      this.variant = variant ?? new Dictionary<string, int?>() { { "default", null } };
+      this.position = position;
+      defaultName = defaultVariant == null ? name : $"{name}_{defaultVariant}";
+      canvasImage = new Rectangle()
+      {
+        Width = Config.ItemSize,
+        Height = Config.ItemSize,
+        Fill = imageBrush
+      };
+      // set default sprite
+      images["default"] = GetImage(GetImageUri("default"));
+      currVariant = "default";
+      imageBrush.ImageSource = images[currVariant];
+      LoadImages();
+    }
+    protected virtual void UpdateImage()
+    {
+      imageBrush.ImageSource = images[currVariant];
+    }
+    public virtual void SwitchImage(string variant)
+    {
+      currVariant = variant;
+      UpdateImage();
+      DrawUpdate();
+    }
+    protected virtual void LoadImages()
+    {
+      // Mount all variant images
+      foreach (KeyValuePair<string, int?> pair in variant)
+      {
+        images[pair.Key] = GetImage(GetImageUri(pair.Key));
+      }
+      DrawElement();
+    }
+    public virtual void DrawElement(int zIndex = 1)
+    {
+      Renderer.DrawElement(canvasImage, position.CanvasX, position.CanvasY, zIndex);
+    }
+    public void DrawUpdate()
+    {
+      Debug.WriteLine($"Update x: {position.CanvasX} y: {position.CanvasY}");
+      Renderer.PositionElement(canvasImage, position.CanvasX, position.CanvasY);
+    }
+    protected static BitmapImage GetImage(Uri uri)
+    {
+      return new BitmapImage(uri);
+    }
+    protected Uri GetImageUri(string variantName)
+    {
+      return GetImageUriFromName(variantName == "default" ? defaultName : $"{name}_{variantName}");
+    }
+    private static Uri GetImageUriFromName(string name)
+    {
+      return new Uri("Resources/" + name + Config.ImageExt, UriKind.Relative);
+    }
+
+    public void Dispose()
+    {
+      Renderer.RemoveElement(canvasImage);
+    }
+  }
+}
