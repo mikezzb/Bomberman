d752b8bb645a6cc00beca7750f93ef71e0a75ff2
Mike Zhou <mikezhoudev@gmail.com>
2022-11-21 20:46:06 +0800

    chore: add direction type

--- Bomberman/GameEngine/Config.cs
+++ Bomberman/GameEngine/Config.cs
@@ -1,4 +1,6 @@
-﻿namespace Bomberman.GameEngine
+﻿using System.Collections.Generic;
+
+namespace Bomberman.GameEngine
 {
   public static class Config
   {
@@ -7,6 +9,19 @@
     public static readonly int Width = 31;
     public static readonly string ImageExt = ".png";
     public static readonly string Map = Resources.map;
+    public static readonly int WalkFrames = 8;
+    public static readonly int WalkFrameDuration = 30;
+    /// <summary>
+    /// Time to walk 1 unit in ms
+    /// </summary>
+    public static readonly int WalkDuration = WalkFrameDuration * WalkFrames;
+    public static readonly Dictionary<Enums.Direction, string> DirectionName = new()
+    {
+      { Enums.Direction.Up, "up" },
+      { Enums.Direction.Down, "down" },
+      { Enums.Direction.Left, "left" },
+      { Enums.Direction.Right, "right" },
+    };
   };
   namespace Enums
   {
@@ -20,6 +35,13 @@
       Started,
       Paused,
       Ended,
+    }
+    public enum Direction
+    {
+      Up,
+      Down,
+      Left,
+      Right,
     }
   }
 }
