3af56200771cc3db8c82989d956407af8efde8f0
Mike Zhou <mikezhoudev@gmail.com>
2022-11-21 23:21:23 +0800

    feat: int point & before next move event

---
+++ Bomberman/GameEngine/Common.cs
@@ -0,0 +1,28 @@
+﻿using Bomberman.GameEngine.Enums;
+using System;
+using System.Windows.Input;
+
+namespace Bomberman.GameEngine
+{
+  public class IntPoint
+  {
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public IntPoint(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+  }
+  public class BeforeNextMoveEventArgs : EventArgs
+  {
+    public IntPoint From { get; private set; }
+    public IntPoint To { get; private set; }
+    public Boolean Cancel { get; set; }
+    public BeforeNextMoveEventArgs(IntPoint from, IntPoint to)
+    {
+      From = from;
+      To = to;
+    }
+  }
+}
