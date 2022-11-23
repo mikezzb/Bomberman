d7ae4a86b8789d9367c98ccb5f13b403be7ca995
Mike Zhou <mikezhoudev@gmail.com>
2022-11-23 18:01:26 +0800

    refactor: movable map object

--- Bomberman/GameEngine/Graphics/AnimatedSprite.cs
+++ Bomberman/GameEngine/Graphics/AnimatedSprite.cs
@@ -6,19 +6,16 @@ using System.Windows.Media.Imaging;
 
 namespace Bomberman.GameEngine.Graphics
 {
-  public class AnimatedSprite : Sprite
+  public class AnimatedSprite : Sprite, IUpdatable
   {
-    private Direction? currDir;
-    private Direction? nextDir;
-    private bool stopAfterMove;
-    private double walkSize;
-    public bool Moving { get => currDir != null; }
-    public Direction? CurrDir { get => currDir; }
+    /// <summary>
+    /// If movable, need update every frame, else only update image per some frames
+    /// </summary>
+    private readonly bool movable = false;
+    private bool running = false;
+    private int imageFrames = 2;
     private int? currFrameNum;
-    private readonly FrameTimer imageTimer;
-    private readonly FrameTimer moveTimer;
     private readonly MovableGridPosition movablePosition;
-    private readonly EventHandler<BeforeNextMoveEventArgs> onBeforeNextMove;
     /// <summary>
     /// key: variantName, value: frames of the variant
     /// </summary>
@@ -28,153 +25,28 @@ namespace Bomberman.GameEngine.Graphics
       Dictionary<string, int?>? variant,
       string? defaultVariant,
       ref GridPosition position,
-      EventHandler<BeforeNextMoveEventArgs> onBeforeNextMove,
-      int zIndex = 1
+      int zIndex = 1,
+      bool movable = false
       ) : base(name, variant, defaultVariant, ref position, zIndex)
     {
+      this.movable = movable;
       // set default sprite
       animatedImages["default"] = animatedImages[defaultVariant ?? name];
-      this.onBeforeNextMove = onBeforeNextMove;
       movablePosition = (MovableGridPosition)position;
-      imageTimer = new FrameTimer(100, 2, OnImageTick);
-      moveTimer = new FrameTimer(Config.WalkFrameDuration, Config.WalkFrames, OnMoveTick, OnMoveEnded);
     }
     /// <summary>
-    /// Move sprite until stop move
+    /// Update the frame image every 4 frames, update position every frame if movable
     /// </summary>
-    /// <param name="dir"></param>
-    /// <param name="walkSize"></param>
-    public void Move(Direction dir, double walkSize)
+    public void Update(int frameNum)
     {
-      stopAfterMove = false;
-      if (currDir != null)
+      if (!running) return;
+      if (!movable)
       {
-        Debug.WriteLine($"[NEXT MOVE]: {dir}");
-        nextDir = dir;
-        return;
+        if (frameNum % 4 != 0) return;
+        currFrameNum += (currFrameNum) % imageFrames;
+        UpdateImage();
       }
-      StartMove(dir, walkSize);
-    }
-    public void ResetMove(Direction dir, double walkSize)
-    {
-      StartMove(dir, walkSize, true);
-    }
-    public void StartMove(Direction dir, double walkSize, bool skipCheck = false)
-    {
-      if (!skipCheck && CanMove(dir).Cancel) return;
-      // Debug.WriteLine($"[MOVE]: {dir}");
-      currDir = dir;
-      SwitchMoveImage();
-      StartAnimation();
-      this.walkSize = walkSize;
-    }
-    public IntPoint PostMovePosition(Direction dir)
-    {
-      return movablePosition.PostMovePosition(dir);
-    }
-    /// <summary>
-    /// Stop moving sprite
-    /// </summary>
-    /// <param name="dir"></param>
-    public void StopMove(Direction dir)
-    {
-      stopAfterMove = nextDir == null
-        ? currDir == dir : nextDir == dir;
-      if (stopAfterMove)
-      {
-        // Debug.WriteLine($"[{dir} UP]: STOP AFTER DONE");
-      }
-    }
-    public void StopMoveNow()
-    {
-      StopAnimation();
-      currDir = null;
-      nextDir = null;
-    }
-    /// <summary>
-    /// Called every frame to move image on canvas (for walking animation)
-    /// </summary>
-    /// <param name="frameNum"></param>
-    private void OnMoveTick(int frameNum)
-    {
-      if (currDir == null) return;
-      // Debug.WriteLine($"[{currDir}]-{frameNum}");
-      // shift position & draw
-      movablePosition.Move(currDir, walkSize);
       DrawUpdate();
-    }
-    private BeforeNextMoveEventArgs CanMove(Direction dir)
-    {
-      IntPoint from = movablePosition.Position;
-      IntPoint to = movablePosition.PostMovePosition(dir);
-      BeforeNextMoveEventArgs e = new(from, to);
-      onBeforeNextMove(this, e);
-      return e;
-    }
-    /// <summary>
-    /// Check game state once a 1 unit walk is done
-    /// <para>- If scheduled next walk, continue move</para>
-    /// <para>- If stop after move, stop move</para>
-    /// </summary>
-    public void OnMoveEnded()
-    {
-      movablePosition.FinishMove();
-      if (nextDir != null)
-      {
-        ContinueMove();
-      }
-      else if (stopAfterMove)
-      {
-        StopMove();
-      }
-      else
-      {
-        ContinueMove();
-      }
-    }
-    /// <summary>
-    /// Continue to move
-    /// <para>- Cond 1: if received next direction command during previous 1 unit walk</para>
-    /// <para>- Cond 2: if current direction NOT key up yet</para>
-    /// </summary>
-    public void ContinueMove()
-    {
-      BeforeNextMoveEventArgs e = CanMove((Direction)(nextDir ?? currDir));
-      if (e.Cancel)
-      {
-        nextDir = null;
-        if (e.TurnDirection == null)
-        {
-          Debug.WriteLine("[stop]: CONTINUE CANCELLED");
-          StopMove();
-        }
-      }
-      else if (nextDir != null)
-      {
-        // Debug.WriteLine("[stop]: CONTINUE NEXT DIR");
-        bool sameDir = currDir == nextDir;
-        currDir = nextDir;
-        if (!sameDir) SwitchMoveImage();
-        nextDir = null;
-      }
-      else
-      {
-        // Debug.WriteLine("[stop]: CONTINUE TILL KEYUP");
-      }
-    }
-    private void StopMove()
-    {
-      Debug.WriteLine("[stop]: STOP");
-      currDir = null;
-      StopAnimation();
-    }
-    /// <summary>
-    /// Display image for the move direction
-    /// </summary>
-    private void SwitchMoveImage()
-    {
-      string directionName = Config.DirectionName[(Direction)currDir];
-      SwitchImage(directionName);
     }
     /// <summary>
     /// Override update image to support animated image update
@@ -205,22 +77,11 @@ namespace Bomberman.GameEngine.Graphics
     }
     public void StartAnimation()
     {
-      imageTimer.Start();
-      moveTimer.Start();
+      running = true;
     }
     public void StopAnimation()
     {
-      imageTimer.Stop();
-      moveTimer.Stop();
-    }
-    private void OnImageTick(int frameNum)
-    {
-      if (currFrameNum != frameNum)
-      {
-        currFrameNum = frameNum;
-        UpdateImage();
-      }
-      DrawUpdate();
+      running = false;
     }
     /// <summary>
     /// Load both static & animated images
--- Bomberman/GameEngine/MapObjects/MovableMapObject.cs
+++ Bomberman/GameEngine/MapObjects/MovableMapObject.cs
@@ -1,8 +1,8 @@
 ﻿using Bomberman.GameEngine.Enums;
+using Bomberman.GameEngine.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System;
-using Bomberman.GameEngine.Graphics;
 
 namespace Bomberman.GameEngine.MapObjects
 {
@@ -13,43 +13,187 @@ namespace Bomberman.GameEngine.MapObjects
   /// <item>Display (Image)</item>
   /// </list>
   /// </summary>
-  public abstract class MovableMapObject : MapObject
+  internal abstract class MovableMapObject : MapObject
   {
     private double speed;
-    private readonly AnimatedSprite animatedSprite;
-    private readonly MovableGridPosition movablePosition;
+    protected readonly AnimatedSprite animatedSprite;
+    protected readonly MovableGridPosition movablePosition;
+    private Direction? currDir;
+    private Direction? nextDir;
+    private bool stopAfterMove;
+    public bool Moving { get => currDir != null; }
+    public Direction? CurrDir { get => currDir; }
     public event EventHandler<BeforeNextMoveEventArgs> BeforeNextMove;
 
     public MovableGridPosition MovablePosition { get => movablePosition; }
-    protected MovableMapObject(int x, int y, string name, Dictionary<string, int?>? variant, string? defaultVariant, double speed = 1.0) : base()
+    protected MovableMapObject(int x, int y, string name, Dictionary<string, int?>? variant, string? defaultVariant, double speed = 1.0, int zIndex = 2) : base()
     {
       this.speed = speed;
       position = new MovableGridPosition(x, y);
-      sprite = new AnimatedSprite(name, variant, defaultVariant, ref position, OnBeforeNextMove);
+      sprite = new AnimatedSprite(name, variant, defaultVariant, ref position, zIndex, true);
       animatedSprite = (AnimatedSprite)sprite;
       movablePosition = (MovableGridPosition)position;
-      Debug.WriteLine($"Speed {this.speed} | {NormalizedWalkSize}");
+      Debug.WriteLine($"Speed {this.speed} | {WalkSize}");
     }
-    private double NormalizedWalkSize { get => (speed / Config.WalkFrames); }
+    private double WalkSize { get => (speed / Config.WalkFrames); }
+    public IntPoint PostMovePosition(Direction dir)
+    {
+      return movablePosition.PostMovePosition(dir);
+    }
     /// <summary>
-    /// Each walk is 1 unit only
+    /// Move image on canvas (for walking animation)
+    /// </summary>
+    /// <param name="frameNum"></param>
+    public override void Update(int frameNum)
+    {
+      // nothing to update if not moving
+      if (currDir == null) return;
+      // shift position & draw
+      movablePosition.Move(currDir, WalkSize);
+      animatedSprite.Update(frameNum);
+      if (frameNum % Config.WalkFrames == 0) OnMoveEnded();
+    }
+
+    /// <summary>
+    /// Move sprite until stop move
+    /// <para>Each walk is 1 unit only</para>
     /// </summary>
     public void Move(Direction dir)
     {
-      animatedSprite.Move(dir, NormalizedWalkSize);
+      stopAfterMove = false;
+      if (currDir != null)
+      {
+        Debug.WriteLine($"[NEXT MOVE]: {dir}");
+        nextDir = dir;
+        return;
+      }
+      StartMove(dir);
     }
+    public void StartMove(Direction dir, bool skipCheck = false)
+    {
+      if (!skipCheck && CanMove(dir).Cancel) return;
+      // Debug.WriteLine($"[MOVE]: {dir}");
+      currDir = dir;
+      SwitchMoveImage();
+      animatedSprite.StartAnimation();
+    }
+    public void ResetMove(Direction dir)
+    {
+      StartMove(dir, true);
+    }
+    /// <summary>
+    /// Stop moving sprite
+    /// </summary>
+    /// <param name="dir"></param>
     public void StopMove(Direction dir)
     {
-      animatedSprite.StopMove(dir);
+      stopAfterMove = nextDir == null
+        ? currDir == dir : nextDir == dir;
+      if (stopAfterMove)
+      {
+        // Debug.WriteLine($"[{dir} UP]: STOP AFTER DONE");
+      }
     }
-    public IntPoint PostMovePosition(Direction dir)
+    public void StopMoveNow()
     {
-      return animatedSprite.PostMovePosition(dir);
+      animatedSprite.StopAnimation();
+      currDir = null;
+      nextDir = null;
     }
-    public void OnBeforeNextMove(object sender, BeforeNextMoveEventArgs e)
+    public virtual void Dead()
+    {
+      StopMoveNow();
+      animatedSprite.SwitchImage("dead");
+    }
+    public virtual bool OverlapsWith(MovableMapObject obj)
+    {
+      return movablePosition.IntersectsWith(obj.movablePosition);
+    }
+    private BeforeNextMoveEventArgs CanMove(Direction dir)
+    {
+      IntPoint from = movablePosition.Position;
+      IntPoint to = movablePosition.PostMovePosition(dir);
+      BeforeNextMoveEventArgs e = new(from, to);
+      InvokeBeforeNextMove(e);
+      return e;
+    }
+
+    public virtual void InvokeBeforeNextMove(BeforeNextMoveEventArgs e)
     {
       BeforeNextMove?.Invoke(this, e);
     }
+
+
+    /// <summary>
+    /// Check game state once a 1 unit walk is done
+    /// <para>- If scheduled next walk, continue move</para>
+    /// <para>- If stop after move, stop move</para>
+    /// </summary>
+    public void OnMoveEnded()
+    {
+      movablePosition.FinishMove();
+      if (nextDir != null)
+      {
+        ContinueMove();
+      }
+      else if (stopAfterMove)
+      {
+        StopMove();
+      }
+      else
+      {
+        ContinueMove();
+      }
+    }
+    /// <summary>
+    /// Continue to move
+    /// <para>- Cond 1: if received next direction command during previous 1 unit walk</para>
+    /// <para>- Cond 2: if current direction NOT key up yet</para>
+    /// </summary>
+    public void ContinueMove()
+    {
+      BeforeNextMoveEventArgs e = CanMove((Direction)(nextDir ?? currDir));
+      if (e.Cancel)
+      {
+        nextDir = null;
+        if (e.TurnDirection == null)
+        {
+          Debug.WriteLine("[stop]: CONTINUE CANCELLED");
+          StopMove();
+        }
+      }
+      else if (nextDir != null)
+      {
+        // Debug.WriteLine("[stop]: CONTINUE NEXT DIR");
+        bool sameDir = currDir == nextDir;
+        currDir = nextDir;
+        if (!sameDir) SwitchMoveImage();
+        nextDir = null;
+      }
+      else
+      {
+        // Debug.WriteLine("[stop]: CONTINUE TILL KEYUP");
+      }
+    }
+    /// <summary>
+    /// Stop immediately, only call after a walk is finished
+    /// </summary>
+    private void StopMove()
+    {
+      Debug.WriteLine("[stop]: STOP");
+      currDir = null;
+      animatedSprite.StopAnimation();
+    }
+    /// <summary>
+    /// Display image for the move direction
+    /// </summary>
+    public void SwitchMoveImage()
+    {
+      string directionName = Config.DirectionName[(Direction)currDir];
+      animatedSprite.SwitchImage(directionName);
+    }
+
+    // speed
     protected void SetSpeed(double speed)
     {
       this.speed = speed;
