1ebeca79296aebd1d9248bcc6b21afb3dc92309e
Mike Zhou <mikezhoudev@gmail.com>
2022-11-26 01:14:44 +0800

    chore: cleanup

--- Bomberman/GameEngine/MapObjects/Bomb.cs
+++ Bomberman/GameEngine/MapObjects/Bomb.cs
@@ -59,6 +59,10 @@ namespace Bomberman.GameEngine.MapObjects
         }
       }
     }
+    /// <summary>
+    /// Explode logic
+    /// </summary>
+    /// <param name="manual"></param>
     private void Explode(bool manual)
     {
       // if ticker triggered same time with manual, prevent explode twice
@@ -77,7 +81,7 @@ namespace Bomberman.GameEngine.MapObjects
       ExplodeEvent?.Invoke(this, new EventArgs());
     }
     /// <summary>
-    /// Manual explode
+    /// Manual explode method
     /// </summary>
     public void Explode()
     {
@@ -116,11 +120,6 @@ namespace Bomberman.GameEngine.MapObjects
       explosionVerticalRect = new(Position.X, minY, 1, height);
       Debug.WriteLine($"[EXPL HORI]: {explosionHorizontalRect}");
       Debug.WriteLine($"[EXPL VERT]: {explosionVerticalRect}");
-      // [DEBUG] draw
-      // Brush b = new SolidColorBrush(Colors.Black);
-      // Brush r = new SolidColorBrush(Colors.Red);
-      // Graphics.Renderer.DrawRectangle(b, minX, Position.Y, width, 1);
-      // Graphics.Renderer.DrawRectangle(r, Position.X, minY, 1, height);
     }
     private void UpdateExtreme(IntPoint pos)
     {
@@ -155,43 +154,10 @@ namespace Bomberman.GameEngine.MapObjects
     {
       if (Exploded) return false;
       GameEngineRect objRect = obj.MovablePosition.Rect;
-      // Debug.WriteLine($"[OBJ]: {objRect}");
-      // Debug.WriteLine($"[HORI]: {explosionHorizontalRect}");
-      // Debug.WriteLine($"[VERT]: {explosionVerticalRect}");
       bool horiIntersects = objRect.OverlapsWith(explosionHorizontalRect);
-      // Debug.WriteLineIf(horiIntersects, "Horizontal intersects");
       if (horiIntersects) return true;
       bool vertIntersects = objRect.OverlapsWith(explosionVerticalRect);
-      // Debug.WriteLineIf(horiIntersects, "Vertical intersects");
       return vertIntersects;
-      /*
-       * 
-       * 
-      Rect objRect = obj.MovablePosition.Rect;
-      Debug.WriteLine($"[OBJ]: {objRect}");
-      bool horiIntersects = objRect.IntersectsWith(explosionHorizontalRect);
-      Debug.WriteLineIf(horiIntersects, "Horizontal intersects");
-      if (horiIntersects) return true;
-      bool vertIntersects = objRect.IntersectsWith(explosionVerticalRect);
-      Debug.WriteLineIf(horiIntersects, "Vertical intersects");
-      return vertIntersects;
-       * 
-       * 
-      Debug.WriteLine($"Explosion range: X:{minX}-{maxX} | Y:{minY}-{maxY}");
-      Debug.WriteLine($"Obj pos: X:{obj.MovablePosition.PreciseX.ToString("0.00")} Y:{obj.MovablePosition.PreciseY.ToString("0.00")}");
-      Debug.WriteLine($"Y delta: {Math.Abs(Position.Y - obj.MovablePosition.PreciseY)} X delta: {Math.Abs(Position.X - obj.MovablePosition.PreciseX)}");
-      bool horiIntersects =
-         obj.MovablePosition.PreciseX >= minX &&
-         obj.MovablePosition.PreciseX < maxX &&
-         Math.Abs(Position.Y - obj.MovablePosition.PreciseY) < 1; // on same Y
-      Debug.WriteLineIf(horiIntersects, "Horizontal intersects");
-      if (horiIntersects) return true;
-      bool vertIntersects =
-        obj.MovablePosition.PreciseY >= minY &&
-        obj.MovablePosition.PreciseY < maxY &&
-         Math.Abs(Position.X - obj.MovablePosition.PreciseX) < 1; // on same X
-      Debug.WriteLineIf(horiIntersects, "Vertical intersects");
-       */
     }
   }
 }
